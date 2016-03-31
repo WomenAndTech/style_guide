5a11d05920116b2f346cb998e5ed85557afa39fc
Raymond Kao <ray@peopleandcode.com>
2016-03-31 11:17:40 -0400

    modified default gulp task to copy *.html files and convert sass files at beginning of task, then watch for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// Convert all *.sass files in project src to CSS files into the dev folder (tmp)
 gulp.task('sass-dev', function(){
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./tmp/css'))
 });
 
-gulp.task('default', function() {
+// Copy all *.html files in project src into the dev folder (tmp)
+gulp.task('copy-index-dev', function(){
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./tmp/'));
+});
+
+// Run 'copy-index-dev' and 'sass-dev' tasks first as dependencies before running async tasks in function()
+gulp.task('default', ['copy-index-dev', 'sass-dev'], function(){
+  // watch for sass file changes in directory and run 'sass-dev'
   gulp.watch('./src/sass/**/*.scss', ['sass-dev']);
+  // watch for html file changes in directory and run 'copy-index-dev'
+  gulp.watch('./src/**/*.html', ['copy-index-dev']);
 });
