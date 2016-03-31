cbd22b271e936aed5132afe56066aa160a51bff0
Raymond Kao <ray@peopleandcode.com>
2016-03-31 12:14:36 -0400

    added init-dev task to remove the tmp folder and prep for new build in dev environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
 
 // Convert all *.sass files in project src to CSS files into the dev folder (tmp)
 gulp.task('sass-dev', function(){
@@ -14,8 +15,12 @@ gulp.task('copy-index-dev', function(){
     .pipe(gulp.dest('./tmp/'));
 });
 
+gulp.task('init-dev', function(){
+  del('./tmp');
+});
+
 // Run 'copy-index-dev' and 'sass-dev' tasks first as dependencies before running async tasks in function()
-gulp.task('default', ['copy-index-dev', 'sass-dev'], function(){
+gulp.task('default', ['init-dev', 'copy-index-dev', 'sass-dev'], function(){
   // watch for sass file changes in directory and run 'sass-dev'
   gulp.watch('./src/sass/**/*.scss', ['sass-dev']);
   // watch for html file changes in directory and run 'copy-index-dev'
