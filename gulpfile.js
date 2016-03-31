f14c7b78499293e5a93362de821609bc97f286e9
Raymond Kao <ray@peopleandcode.com>
2016-03-31 19:42:39 -0400

    added browser-sync for live reloading on file saves, and moved the src and destination strings into variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var del = require('del');
+var browserSync = require('browser-sync');
+
+var srcFolder = "./src";
+var destFolder = "./tmp";
 
 // Convert all *.sass files in project src to CSS files into the dev folder (tmp)
 gulp.task('sass-dev', function(){
-  return gulp.src('./src/sass/**/*.scss')
+  return gulp.src(srcFolder + '/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./tmp/css'))
+    .pipe(gulp.dest(destFolder + '/css'))
 });
 
 // Copy all *.html files in project src into the dev folder (tmp)
 gulp.task('copy-index-dev', function(){
-  return gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./tmp/'));
+  return gulp.src(srcFolder + '/**/*.html')
+    .pipe(gulp.dest(destFolder + '/'));
 });
 
 gulp.task('init-dev', function(){
-  del('./tmp');
+  del(destFolder);
+});
+
+gulp.task('browserSync', function(){
+
+  browserSync.init({
+    server: {
+      baseDir: destFolder
+    }
+  });
+
+  gulp.watch(destFolder + "/**/*.*").on('change', browserSync.reload);
 });
 
 // Run 'copy-index-dev' and 'sass-dev' tasks first as dependencies before running async tasks in function()
-gulp.task('default', ['init-dev', 'copy-index-dev', 'sass-dev'], function(){
+gulp.task('default', ['init-dev', 'copy-index-dev', 'sass-dev', 'browserSync'], function(){
   // watch for sass file changes in directory and run 'sass-dev'
-  gulp.watch('./src/sass/**/*.scss', ['sass-dev']);
+  gulp.watch(srcFolder + '/sass/**/*.scss', ['sass-dev']);
   // watch for html file changes in directory and run 'copy-index-dev'
-  gulp.watch('./src/**/*.html', ['copy-index-dev']);
+  gulp.watch(srcFolder + '/**/*.html', ['copy-index-dev']);
 });
