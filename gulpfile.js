68223b9e3e1a6e84add755c91c0967e7b5034e47
Raymond Kao <ray@peopleandcode.com>
2016-03-31 10:54:33 -0400

    Started basic gulp task: convert SASS to CSS in development

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass-dev', function(){
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./tmp/css'))
+});
 
 gulp.task('default', function() {
-  
+  gulp.watch('./src/sass/**/*.scss', ['sass-dev']);
 });
