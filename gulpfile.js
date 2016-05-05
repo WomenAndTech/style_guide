73d96bb7aa79b2dd8cd41262460279047ed6e5fd
Raymond Kao <ray@peopleandcode.com>
2016-05-05 11:45:22 -0400

    Fixed #1 Updated/Rewrote gulpfile.js so that tasks that need to write to the dev folder (tmp) need to wait for 'clean-tmp' to complete first before writing to the folder.
    Added a new task ```build-dev``` that depends on tasks that write to the dev folder (tmp), and simply returns true when they're completed.
    
    Added a new task ```watch``` that will run all the...watch tasks...basically anything that needs to watch the ```src``` folder for changes.
    
    Updated task ```browsersync``` to depend on ```build-dev``` before running...no point in serving something if it doesn't exist
    
    Renamed most SASS files to partials ('_filename.scss') - a different error message was popping up about $body_font_size not being declared...

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,64 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var del = require('del');
+var clean = require('gulp-clean');
 var browserSync = require('browser-sync');
 
-var srcFolder = "./src";
-var destFolder = "./tmp";
+var srcFolder = "src";
+var destFolder = "tmp";
+
+// Clean up the tmp (aka dev build) folder
+gulp.task('clean-tmp', function(){
+  return gulp.src(destFolder, {read: false})
+    .pipe(clean({force: true}))
+    .pipe(gulp.dest(destFolder));
+});
 
 // Convert all *.sass files in project src to CSS files into the dev folder (tmp)
-gulp.task('sass-dev', function(){
+gulp.task('sass-dev', ['clean-tmp'],function(){
   return gulp.src(srcFolder + '/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest(destFolder + '/css'))
 });
 
 // Copy all *.html files in project src into the dev folder (tmp)
-gulp.task('copy-index-dev', function(){
+gulp.task('copy-index-dev', ['clean-tmp'], function(){
   return gulp.src(srcFolder + '/**/*.html')
-    .pipe(gulp.dest(destFolder + '/'));
+    .pipe(gulp.dest(destFolder));
 });
 
-gulp.task('init-dev', function(){
-  del(destFolder);
+// Copy all assets over to dev folder (tmp)
+gulp.task('copy-assets-dev', ['clean-tmp'], function(){
+  return gulp.src(srcFolder + '/assets/**/*.*')
+    .pipe(gulp.dest(destFolder + '/assets'));
 });
 
-gulp.task('browserSync', function(){
-
-  browserSync.init({
-    server: {
-      baseDir: destFolder
-    }
-  });
-
-  gulp.watch(destFolder + "/**/*.*").on('change', browserSync.reload);
+// Runs all the development build requirements, then just returns true when they're finished
+gulp.task('dev-build', ['copy-index-dev', 'sass-dev', 'copy-assets-dev'], function(){
+  return true;
 });
 
-// Run 'copy-index-dev' and 'sass-dev' tasks first as dependencies before running async tasks in function()
-gulp.task('default', ['init-dev', 'copy-index-dev', 'sass-dev', 'browserSync'], function(){
+// Observes a series of folder observations and then runs a corresponding task
+gulp.task('watch', ['dev-build'], function(){
   // watch for sass file changes in directory and run 'sass-dev'
   gulp.watch(srcFolder + '/sass/**/*.scss', ['sass-dev']);
   // watch for html file changes in directory and run 'copy-index-dev'
   gulp.watch(srcFolder + '/**/*.html', ['copy-index-dev']);
+  // watch for asset changes
+  gulp.watch(srcFolder + '/assets/**/*.*', ['copy-assets-dev']);
 });
+
+// Auto updates the browser when the dev folder (tmp) gets updated
+gulp.task('browserSync', ['dev-build'], function(){
+  browserSync.init({
+    server: {
+      baseDir: destFolder
+    },
+    // Don't auto open a new browser window...just open it yourself!
+    open: false
+  });
+
+  gulp.watch(destFolder + "/**/*.*", browserSync.reload);
+});
+
+// The main gulp task - runs in order based on any dependencies (if any) on each task
+gulp.task('default', ['clean-tmp', 'dev-build', 'watch', 'browserSync']);
