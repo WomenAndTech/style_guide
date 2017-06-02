ac8d93add3252e88b5b88c6fc20cd2abfade01b0
Christina Truong <hello@christinatruong.com>
2017-06-02 18:38:21 -0400

    Updating style guide documentation.

--- src/js/style-guide.js
+++ src/js/style-guide.js
@@ -13,7 +13,25 @@ $(function() {
     var pageHeight = $(document).height();
     $('#width').html(pageWidth);
     $('#height').html(pageHeight);
+    $('#screen').html();
+    
+    if(pageWidth <= 400){
+      $('#screen').html("xsm");
+    } else if (pageWidth <= 660){
+      $('#screen').html("sm");
+    }
+    else if (pageWidth <= 980){
+      $('#screen').html("med");
+    } else {
+      $('#screen').html("lg");
+    }
   }
   $(document).ready(jqUpdateSize);
   $(window).resize(jqUpdateSize);
+  
+  var $navLink = $('[data-scroll-header] a');
+  $navLink.on("click", function(){
+    $navLink.removeClass("active");
+    $(this).addClass("active");
+  });
 });
